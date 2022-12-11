472439f9a8847aa9f8d79134825fb6ee15831c64
erfanscode <96702228+erfanscode@users.noreply.github.com>
2022-12-12 01:29:54 +0330

    build publish manager in model

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,10 @@ from django.utils import timezone
 from accounts.models import User
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT     = 'DF', 'Draft'
@@ -16,6 +20,9 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status  = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    objects     = models.Manager()
+    published   = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
